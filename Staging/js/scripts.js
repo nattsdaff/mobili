3d4813c04180b6dfcbe9a9c5ec8c5c1f46c5edb5
Damian Bertazza <damian.bertazza@gmail.com>
2018-09-16 11:53:21 -0300

    Correcciones en el header / submenu login

--- Staging/js/scripts.js
+++ Staging/js/scripts.js
@@ -1,22 +1,35 @@
-/* TOGGLE PARA EL MENU RESPONSIVE */
-$(document).ready(function(){
-  $(".toggle").click(function(){
-     $("#mainNav .menu").slideToggle(400, function(){
-         $(this).toggleClass("mobile");
-         $(this).css("display", "");
-     });
-  });
-  $(".toggleusermenu").click(function(){
-     $("#mainNav .usermenu").slideToggle(400, function(){
-         $(this).toggleClass("mobile");
-         $(this).css("display", "");
-     });
-  });
+/* TOGGLE PARA EL MENU RESPONSIVE Y MENU DE USUARIO */
+$(document).ready(function () {
+    $(".toggle").click(function () {
+        $("#mainNav .menu").slideToggle(400, function () {
+            $(this).toggleClass("mobile");
+            $(this).css("display", "");
+        });
+    });
+    /* Función para cerrar el menú si se hace click afuera del mismo */
+    $(document).click(function (event) {
+        if (!$(event.target).closest(".menu,.toggle").length) {
+            $("body").find(".menu").addClass("mobile");
+        }
+    });
+
+    $(".toggleUserMenu").click(function () {
+        $("#mainNav .userMenu").fadeToggle(200, function () {
+            $(this).toggleClass("mobile");
+            $(this).css("display", "");
+        });
+    });
+    /* Función para cerrar el menú si se hace click afuera del mismo */
+    $(document).click(function (event) {
+        if (!$(event.target).closest(".userMenu,.toggleUserMenu").length) {
+            $("body").find(".userMenu").addClass("mobile");
+        }
+    });
 });
 
 
 /* SETTINGS PARA EL SLIDER DE PRODUCTOS DESEADOS */
-$(document).ready(function() {
+$(document).ready(function () {
     $(".deseados-slider").slick({
         infinite: true,
         arrows: true,
@@ -26,35 +39,35 @@ $(document).ready(function() {
         slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
         nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
-        mobileFirst:true,
+        mobileFirst: true,
         responsive: [
-        {
-          breakpoint: 400,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-          }
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
         },
-        {
-          breakpoint: 575,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          }
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
         },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 2
-          }
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2
+                }
         },
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 5,
-            slidesToScroll: 3
-          }
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 3
+                }
         }
       ]
     });
