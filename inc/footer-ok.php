45a5d58d8ef26d517a3df834a6eb9b3911e8e9e4
nattsdaff <ndaffunchio@gmail.com>
2018-10-12 22:37:44 -0300

    new footer

---
+++ inc/footer-ok.php
@@ -0,0 +1,17 @@
+<!--FOOTER-->
+<footer>
+    <div class="altMenu">
+        <img src="img/logo-white.png" alt="Mobili Logo">
+        <ul>
+            <li><a href="#">Servicio al cliente</a></li>
+            <li><a href="#">Nuestra empresa</a></li>
+            <li><a href="preguntas-frecuentes.php">Preguntas frecuentes</a></li>
+            <li><a href="#">Envios</a></li>
+            <li><a href="#">Cambios y devoluciones</a></li>
+        </ul>
+    </div>
+    <div class="copy">
+        <p>2018&copy; Todos los derechos reservados.</p>
+    </div>
+</footer>
+<!--END OF FOOTER-->
--- inc/footer.php
+++ inc/footer.php
@@ -1,17 +1,38 @@
 <!--FOOTER-->
 <footer>
-    <div class="altMenu">
-        <img src="img/logo-white.png" alt="Mobili Logo">
-        <ul>
-            <li><a href="#">Servicio al cliente</a></li>
-            <li><a href="#">Nuestra empresa</a></li>
-            <li><a href="preguntas-frecuentes.php">Preguntas frecuentes</a></li>
-            <li><a href="#">Envios</a></li>
-            <li><a href="#">Cambios y devoluciones</a></li>
-        </ul>
+
+    <div class="container">
+        
+        <!--NEWSLETTER-->
+        <section class="newsletter">
+            <div class="container-newsletter">
+                <h3 class="alt-title">Suscribite al newsletter</h3>
+                <p class="newsletter-info">Recibí nuestras ofertas y novedades directamente en tu e-mail.</p>
+                <form action="#" method="post" class="form">
+                    <div class="form-item">
+                        <input type="email" name="email" placeholder="Ingrese su e-mail" required class="form-field" value="email">
+                    </div>
+                    <input type="submit" value="Enviar" class="submit-btn">
+                </form>
+            </div>
+        </section>
+        <!--END OF NEWSLETTER-->
+
+        <div class="alt-menu">
+            <img src="img/logo-white.png" alt="Mobili Logo">
+            <ul>
+                <li><a href="#">Servicio al cliente</a></li>
+                <li><a href="#">Nuestra empresa</a></li>
+                <li><a href="preguntas-frecuentes.php">Preguntas frecuentes</a></li>
+                <li><a href="#">Envios</a></li>
+                <li><a href="#">Cambios y devoluciones</a></li>
+            </ul>
+            <br>
+            <div class="copy"><p>2018&copy; Todos los derechos reservados.</p></div>
+        </div>
+
     </div>
-    <div class="copy">
-        <p>2018&copy; Todos los derechos reservados.</p>
-    </div>
+
+
 </footer>
 <!--END OF FOOTER-->
