765ec8f99797b0983bf5fc22e4df3451dd0d11c7
nattsdaff <ndaffunchio@gmail.com>
2018-11-09 20:35:29 -0300

    Update de modelos

--- config.php
+++ config.php
@@ -7,22 +7,6 @@ if(!$db){
     session_start();
 }
 
-// /* Base de datos Mysql */
-// $dsn = 'mysql:host=127.0.0.1;dbname=;port=8889;charset=UTF8';
-// $db_user = 'root';
-// $db_pass = 'root';
-// $error = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-
-// try {
-//     $db = new PDO($dsn, $db_user, $db_pass, $error);
-// }
-// catch (PDOException $Exception) {
-//     echo $Exception->getMessage();
-// }
-
-/* "Base de datos" para Json */
-// $jdb = file_get_contents("datos.json");
-
 require('Classes/DB.php');
 require('Classes/Mysql.php');
 require('Classes/Json.php');
