4c98446ba5445b16c77749766aa9f68925ae93b2
Tomás Peralta <tomiperalta1@gmail.com>
2018-11-09 22:37:56 -0300

    Validaciones

--- Classes/Validar.php
+++ Classes/Validar.php
@@ -3,64 +3,114 @@
 class Validar
 {
     public static function validarSiExiste($email, $db)
-    {   
+    {
         try {
             $query = $db->prepare('SELECT * FROM usuarios WHERE email=:email');
-            
+
             $query->bindValue(':email', $email, PDO::PARAM_STR);
-            
+
             $query->execute();
             $resultado = $query->fetch(PDO::FETCH_ASSOC);
             return $resultado;
-        } 
+        }
         catch (PDOException $Exception) {
             echo $Exception->getMessage();
         }
     }
-    
+
     private static function passwordConfirm(User $user, $data)
     {
         $password1 = $user->getPassword();
         $password2 = $data['passwordConfirm'];
-        
+
         return $password1 == $password2;
     }
     public static function validacionRegistro($user, $data, $db)
     {
-        
+
         $errores=[];
 
         if (strlen($user->getNombre())<2) {
-        $errores["nombre"]="Nombre no valido";
+        $errores["nombre"]="Nombre no válido";
+        } elseif (!preg_match("/^[a-zA-Z áéíóúÁÉÍÓÚñÑüÜ]*$/", ($user->getNombre()))) {
+          $errores["nombre"]="Sólo se permiten letras y espacios en blanco";
+        } else {
+          $nombreSinEspacios = trim($user->getNombre());
+          $user->setNombre($nombreSinEspacios);
         }
+
         if (strlen($user->getApellido())<2) {
-        $errores["apellido"]="Apellido no valido";
+        $errores["apellido"]="Apellido no válido";
+        } elseif (!preg_match("/^[a-zA-Z áéíóúÁÉÍÓÚñÑüÜ]*$/", ($user->getApellido()))) {
+          $errores["apellido"]="Sólo se permiten letras y espacios en blanco";
+        } else {
+          $apellidoSinEspacios = trim($user->getApellido());
+          $user->setApellido($apellidoSinEspacios);
         }
 
         //si es true significa que ya existe el usuario y que no esta disponible
         if (self::validarSiExiste($user->getEmail(), $db)) {
             //Con self::<nombredemetodo> accedemos a metodos estaticos dentro de la misma
-            $errores["email"] ="ya existe este usuario o mail";
+            $errores["email"] ="Ya existe este usuario o email";
 
         }
 
         if (strlen($user->getPassword())<6) {
-            $errores["contrasena"]="Contraseña demasiado cortasss";
+            $errores["password"]="Contraseña demasiado corta";
         }
         if (!self::passwordConfirm($user, $data)) {
             $errores["passwordConfirm"]="No concuerdan las contraseñas";
         }
+
         if (!filter_var($user->getEmail(), FILTER_VALIDATE_EMAIL)) {
             $errores["email"]="no ingreso un email valido";
         }
+
+        if ((date("Y")-($user->getFNacAnio()))<18) {
+            $errores["edad"]="Debe ser mayor de 18 años";
+        }
+
+        if ($user->getTelCod() !== "" && (!is_numeric($user->getTelCod()))){
+          $errores["telcod"]="Código de área inválido";
+        } elseif (strpos($user->getTelCod(), ".")) {
+            $errores["telcod"]="Debe ingresar sólo números";
+        } elseif ($user->getTelCod() !== "" && ((($user->getTelCod())<11) || (($user->getTelCod())>3894) || ((strlen($user->getTelCod()))<2) || ((strlen($user->getTelCod()))>5))) {
+          $errores["telcod"]="Código de área inválido";
+        }
+
+        if ($user->getTelefono() !== "" && (!is_numeric($user->getTelefono()))) {
+            $errores["telefono"]="Teléfono inválido";
+        } elseif (strpos($user->getTelefono(), ".")) {
+            $errores["telefono"]="Debe ingresar sólo números";
+        } elseif ($user->getTelefono() !== "" && ((strlen($user->getTelefono()))<6) || ((strlen($user->getTelefono()))>8)) {
+            $errores["telefono"]="Teléfono inválido";
+        }
+
+        if (($user->getTelCod()=="") && (($user->getTelefono()!==""))) {
+          $errores["telcod"]="Debe completar el código de área";
+        }
+
+        if (($user->getTelCod()!=="") && (($user->getTelefono()==""))) {
+          $errores["telefono"]="Debe completar el teléfono";
+        }
+
+        if ($user->getDni() !== "" && (!is_numeric($user->getDni()))) {
+            $errores["dni"]="DNI inválido";
+        } elseif (strpos($user->getDni(), ".")) {
+            $errores["dni"]="Debe ingresar sólo números";
+        } elseif ($user->getDni() !== "" && (($user->getDni())<2000000) || (($user->getDni())>120000000)) {
+            $errores["dni"]="DNI inválido";
+        }
+
+
         return $errores;
     }
-    
+
     public static function logearUsuario($datosLogin, $db)
     {
         /*$guardados = getMySQLConfig(); //funciones.php line 10
         $db = newPDO(); //funciones.php line 16*/
-        
+
         $resultado = Mysql::buscarUsuario($datosLogin, $db);
         $error="Datos inválidos";
         if ( $resultado && password_verify($datosLogin["password"], $resultado["password"]) ) {
@@ -87,7 +137,7 @@ class Validar
             }
         }
         return $error;
-    }    
+    }
 }
 
     /* LOGEAR A TRAVES DE JSON */
